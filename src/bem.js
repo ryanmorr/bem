77d94a92c290bb19937a9fc95d5d4932c0487fbd
Ryan Morr <rm.morr@gmail.com>
2017-05-31 15:06:39 -0400

    Initial implementation

---
+++ src/bem.js
@@ -0,0 +1,151 @@
+/**
+ * Common variables
+ */
+const slice = [].slice;
+const elementSeparator = '__';
+const modifierSeparator = '--';
+
+/**
+ * Does an object have the characteristics
+ * of an array-like object
+ *
+ * @param {*} obj
+ * @return {Boolean}
+ * @api private
+ */
+function isArrayLike(obj) {
+    return obj
+        && typeof obj === 'object'
+        && typeof obj.length === 'number'
+        && obj.length >= 0
+        && obj.length % 1 === 0;
+}
+
+/**
+ * Convert an array-like object to
+ * an array
+ *
+ * @param {ArrayLike} obj
+ * @return {Array}
+ * @api private
+ */
+function toArray(obj) {
+    if (Array.from) {
+        return Array.from(obj);
+    }
+    return slice.call(obj);
+}
+
+/**
+ * Get BEM class name from element
+ *
+ * @param {Element} el
+ * @return {String}
+ * @api private
+ */
+function getBEMName(el) {
+    return el.className.split(' ')[0];
+}
+
+/**
+ * Helper class for maninuplating elements
+ * according to the BEM (Block Element Modifier)
+ * methodology
+ *
+ * @class BEM
+ * @api public
+ */
+class BEM {
+
+    /**
+     * Instantiate the class providing either a
+     * selector string, DOM element, or array-like
+     *
+     * @constructor
+     * @param {String|Element|ArrayLike} selector
+     * @param {Element} context (optional)
+     * @api private
+     */
+    constructor(selector, context = document) {
+        if (selector.nodeType) {
+            this.elements = [selector];
+        } else if (isArrayLike(selector)) {
+            this.elements = toArray(selector);
+        } else {
+            this.elements = toArray(context.querySelectorAll(selector));
+        }
+        this.name = getBEMName(this.elements[0]);
+    }
+
+    /**
+     * Find BEM blocks that are decendants
+     * of the currently selected elements
+     *
+     * @param {String} name
+     * @return {BEM}
+     * @api public
+     */
+    block(name) {
+        const elements = this.elements.reduce((els, el) => {
+            const selector = '.' + this.name + ' .' + name;
+            return els.concat(toArray(el.querySelectorAll(selector)));
+        }, []);
+        return bem(elements);
+    }
+
+    /**
+     * Find BEM block-elements that are decendants
+     * of the currently selected elements
+     *
+     * @param {String} name
+     * @return {BEM}
+     * @api public
+     */
+    element(name) {
+        const elements = this.elements.reduce((els, el) => {
+            const selector = '.' + this.name + elementSeparator + name;
+            return els.concat(toArray(el.querySelectorAll(selector)));
+        }, []);
+        return bem(elements);
+    }
+
+    /**
+     * Add a modifier class to the currently
+     * selected elements
+     *
+     * @param {String} modifier
+     * @return {BEM}
+     * @api public
+     */
+    modify(modifier) {
+        return this.elements.forEach((el) => {
+            el.classList.add(this.name + modifierSeparator + modifier);
+        });
+    }
+
+    /**
+     * Remove a modifier class to the currently
+     * selected elements
+     *
+     * @param {String} modifier
+     * @return {BEM}
+     * @api public
+     */
+    unmodify(modifier) {
+        return this.elements.forEach((el) => {
+            el.classList.remove(this.name + modifierSeparator + modifier);
+        });
+    }
+}
+
+/**
+ * Export the `bem` function
+ *
+ * @param {String|Element|ArrayLike} selector
+ * @param {Element} context (optional)
+ * @return {BEM}
+ * @api public
+ */
+export default function bem(selector, context) {
+    return new BEM(selector, context);
+}
--- test/specs/bem.js
+++ test/specs/bem.js
@@ -1,10 +1,97 @@
 /* eslint-disable max-len */
 
 import { expect } from 'chai';
-import sinon from 'sinon';
+import bem from '../../src/bem';
 
 describe('bem', () => {
-    it('', () => {
-        
+    const container = document.createElement('div');
+    container.id = 'container';
+
+    before(() => {
+        document.body.appendChild(container);
+    });
+
+    after(() => {
+        document.body.removeChild(container);
+    });
+
+    beforeEach(() => {
+        container.innerHTML = `
+            <div id="widget-1" class="widget">
+                <div class="widget__header"></div>
+                <div class="widget__body">
+                    <div id="tabs-1" class="tabs"></div>
+                </div>
+                <div class="widget__footer"></div>
+            </div>
+        `;
+    });
+
+    it('should be able to get a BEM block by CSS selector', () => {
+        const widget = bem('.widget');
+
+        expect(widget.elements).to.have.lengthOf(1);
+        expect(widget.elements[0].id).to.equal('widget-1');
+    });
+
+    it('should be able to get a BEM block by passing an element', () => {
+        const widget = bem(document.querySelector('.widget'));
+
+        expect(widget.elements).to.have.lengthOf(1);
+        expect(widget.elements[0].id).to.equal('widget-1');
+    });
+
+    it('should be able to get a BEM block by passing an array/nodelist', () => {
+        const widget = bem(document.querySelectorAll('.widget'));
+
+        expect(widget.elements).to.have.lengthOf(1);
+        expect(widget.elements[0].id).to.equal('widget-1');
+    });
+
+    it('should extract the BEM block/element name from selected element(s)', () => {
+        const widget = bem('.widget');
+
+        expect(widget.name).to.equal('widget');
+    });
+
+    it('should be able to get one or more blocks that are decendants of the currently selected elements', () => {
+        const widget = bem('.widget');
+        const tabs = widget.block('tabs');
+
+        expect(tabs.elements).to.have.lengthOf(1);
+        expect(tabs.elements[0].id).to.equal('tabs-1');
+    });
+
+    it('should be able to get one or more block-elements that are decendants of the currently selected elements', () => {
+        const widget = bem('.widget');
+        const header = widget.element('header');
+
+        expect(header.elements).to.have.lengthOf(1);
+        expect(header.elements[0].className).to.equal('widget__header');
+    });
+
+    it('should be able to add a modifier to the currently selected elements', () => {
+        const widget = bem('.widget');
+        const header = widget.element('header');
+
+        widget.modify('foo');
+        header.modify('bar');
+
+        expect(widget.elements[0].className).to.equal('widget widget--foo');
+        expect(header.elements[0].className).to.equal('widget__header widget__header--bar');
+    });
+
+    it('should be able to remove a modifier from the currently selected elements', () => {
+        const widget = bem('.widget');
+        const header = widget.element('header');
+
+        widget.modify('foo');
+        header.modify('bar');
+
+        widget.unmodify('foo');
+        header.unmodify('bar');
+
+        expect(widget.elements[0].className).to.equal('widget');
+        expect(header.elements[0].className).to.equal('widget__header');
     });
 });
